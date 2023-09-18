addbc00add107d50fbfb3c4c92c0dd5d3a40a276
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2023-09-18 09:38:30 +0530

    Initialized schema and fastapi app

---
+++ backend/models/__init__.py
@@ -0,0 +1,3 @@
+from .auth import *
+from .generic import *
+from .inference import *
---
+++ backend/models/inference.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+from typing import List
+from .generic import Base
+
+class Inference(Base):
+    docstr:str
