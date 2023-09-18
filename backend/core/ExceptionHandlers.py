5d8f196a43f88b71ba1132b00c713e29ee153d05
Mayuresh Agashe <mayureshagashe2002@gmail.com>
2023-09-18 22:48:27 +0530

    Add Config and Exception Handlers

---
+++ backend/core/ExceptionHandlers.py
@@ -0,0 +1,27 @@
+from docguptea import app
+from .Exceptions import *
+
+from fastapi.responses import JSONResponse
+from fastapi.requests import Request
+from fastapi import status
+
+
+
+@app.exception_handler(ExistingUserException)
+async def handle_existing_user_found(request: Request, exec: ExistingUserException):
+    return JSONResponse(status_code=status.HTTP_226_IM_USED,
+                        content=repr(exec)
+                        )
+
+
+@app.exception_handler(InvalidCredentialsException)
+async def handle_login_failed(request: Request, exec: InvalidCredentialsException):
+    return JSONResponse(status_code=status.HTTP_403_FORBIDDEN,
+                        content=repr(exec)
+                        )
+
+@app.exception_handler(InfoNotFoundException)
+async def handle_info_not_found(request: Request, exec: InfoNotFoundException):
+    return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED,
+                        content=repr(exec)
+                        )
