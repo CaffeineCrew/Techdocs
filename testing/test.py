db88ae6b47d6713a5b0719c415b6d221c63a43cb
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2023-09-28 13:17:39 +0530

    cmd tool dev (#10)
    * Update init
    
    * Update router
    
    * Update router
    
    * Update router
    
    * Update router
    
    * Update router
    
    * Update router
    
    * Update router.py
    
    * Update router
    
    * Update vercel.json
    
    * Update vercel.json
    
    * Update router
    
    * Modify AST nodes inplace and write to file

---
+++ testing/test.py
@@ -0,0 +1,21 @@
+def add(a, b):
+    return a + b
+
+
+
+def multiply(a, b):
+    return a * b
+
+def subtract(a, b):
+    return a - b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+
+def func(*args, **kwargs):
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
