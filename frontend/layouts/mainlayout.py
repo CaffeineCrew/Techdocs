c5c24f857d8d28fdaf6f48ebbfb038f896c2869b
Hemanth Sai Garladinne <73033596+HemanthSai7@users.noreply.github.com>
2023-10-03 02:39:16 +0530

    Update mainlayout.py

--- frontend/layouts/mainlayout.py
+++ frontend/layouts/mainlayout.py
@@ -6,7 +6,7 @@ from typing import Callable
 from components import authors, user_greetings, login
 
 def mainlayout(func: Callable):
-    with open("layouts/st_page_layouts.json", "r",encoding='utf-8') as f:
+    with open("frontend/layouts/st_page_layouts.json", "r",encoding='utf-8') as f:
         st_page_layouts = json.load(f)
     st.set_page_config(**st_page_layouts[f"{func.__name__}" if func.__name__ in st_page_layouts.keys() else "home"])
     st.markdown("## :rainbow[Welcome to Techdocs: Where Code Meets Clarity!]🚀")
