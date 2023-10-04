4d20d4438e8d14895febfa2e1249d8eeaa2238b0
Mayuresh Agashe <75118658+mayureshagashe2105@users.noreply.github.com>
2023-10-04 12:32:13 +0530

    Update cli.py

--- techdocs/techdocs/cli.py
+++ techdocs/techdocs/cli.py
@@ -29,28 +29,5 @@ def main(log_info: bool = False):
         """
         )
     
-    # parser.add_argument('--api_key','-k',type=str,required=True,help='API key for Techdocs')
-    
-    # parser.add_argument('--username','-u',type=str,required=True,help='Username for Techdocs')
-
-    # parser.add_argument('--password','-p',type=str,required=True,help='Password for Techdocs')
-
-    # parser.add_argument('--dir','-d',type=str,required=True,help='Root directory to be documented')
-    
-    # # parser.add_argument('--version','-v',action='version',version="%(prog)s 0.0.1")
-
-    # args=parser.parse_args()
-
-    
-
-    # data = {
-    #         "username":config['username'],
-    #         "password":config['password']
-    #         }
-
-    # config.update({"access_token":functools.get_access_token(data)})
-
-    # parse.extract_functions_from_directory(config)
-    
 if __name__ == '__main__':
     main()
