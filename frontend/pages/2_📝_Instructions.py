53c5d8a958cb27fe4ef96e33829cb17a729c505a
HemanthSai7 <hemanth.sai3187@gmail.com>
2023-10-16 18:25:24 +0530

    Render SVG Logo

--- frontend/pages/2_📝_Instructions.py
+++ frontend/pages/2_📝_Instructions.py
@@ -5,15 +5,16 @@ from layouts.mainlayout import mainlayout
 
 @mainlayout
 def instructions():
+
     with open("frontend/content/installation.md", "r",encoding='utf-8') as f:
         instructions = f.read()
 
-    with open("frontend/content/working.md", "r",encoding='utf-8') as f:
+    with open("content/working.md", "r",encoding='utf-8') as f:
         working = f.read()
     
 
     st.markdown("### 📝 :rainbow[Using Techdocs via the CLI]")  
-    st.info("Please use the CLI to generate the documentation for your project. The Streamlit app is just a demo to give the user an idea of the project.")  
+    st.info("Please use the CLI to generate the documentation for your project. The Streamlit app is just a demo to give the user an idea of the project.") 
     
     with st.expander("⚙️ Installation and setup"):
         st.markdown(instructions)
