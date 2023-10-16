710e391c70afe82a6c41a6be6817cdc69ecf3fe5
Hemanth Sai Garladinne <73033596+HemanthSai7@users.noreply.github.com>
2023-10-16 18:30:17 +0530

    Update mainlayout.py

--- frontend/layouts/mainlayout.py
+++ frontend/layouts/mainlayout.py
@@ -9,6 +9,7 @@ def mainlayout(func: Callable):
     with open("frontend/layouts/st_page_layouts.json", "r",encoding='utf-8') as f:
         st_page_layouts = json.load(f)
     st.set_page_config(**st_page_layouts[f"{func.__name__}" if func.__name__ in st_page_layouts.keys() else "home"])
+    add_logo("frontend/images/techdocslogo.svg",svg=True)
     st.markdown("## :rainbow[Welcome to Techdocs: Where Code Meets Clarity!]🚀")
 
     user_greetings()
