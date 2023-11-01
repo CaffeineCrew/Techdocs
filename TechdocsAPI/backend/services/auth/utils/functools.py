469c1d092589b048b99dcb611628bebc6a740d59
Mayuresh Agashe <mayureshagashe2105@users.noreply.huggingface.co>
2023-11-01 16:24:40 +0000

    Update TechdocsAPI/backend/services/auth/utils/functools.py

--- TechdocsAPI/backend/services/auth/utils/functools.py
+++ TechdocsAPI/backend/services/auth/utils/functools.py
@@ -6,4 +6,5 @@ def post_request(url: str, data: Dict[str, Any], headers: Dict[str, str]=None):
     json_data = json.dumps(data)
     headers = headers or {'Content-type': 'application/json', 'Accept': 'application/json'}
     response = requests.post(url, data=data, headers=headers)
+    print(response.json())
     return response.status_code
