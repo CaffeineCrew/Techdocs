5782a92bfb68b53c081ca8f32700ded39e07a0fa
HemanthSai7 <hemanth.sai3187@gmail.com>
2024-05-11 01:13:00 +0530

    Update

--- TechdocsAPI/backend/__init__.py
+++ TechdocsAPI/backend/__init__.py
@@ -26,7 +26,7 @@ try:
     test_conn = DBConnection.get_client().get_server_info()
 
     # send prompt wizardcoderLM-70b-instruct-GGUF model
-    with open("backend/utils/Gemini_Prompt.txt", "r") as f:
+    with open("backend/utils/prompt.txt", "r") as f:
         prompt = f.read()
 
     prompt = PromptTemplate(template=prompt, input_variables=["instruction"])
